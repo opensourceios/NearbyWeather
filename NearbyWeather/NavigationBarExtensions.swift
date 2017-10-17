3ba864c4f4c254457e53f40eaf4a65b4e14ee7d1
erikmartens <erikmartens@me.com>
2017-10-18 00:24:57 +0200

    new uicolor and navbar extensions, several ui (color related) updates

---
+++ NearbyWeather/NavigationBarExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  NavigationBarExtensions.swift
+//  NearbyWeather
+//
+//  Created by Erik Maximilian Martens on 18.10.17.
+//  Copyright © 2017 Erik Maximilian Martens. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationBar {
+    
+    func styleStandard(withTransluscency isTransluscent: Bool, animated: Bool) {
+        isTranslucent = isTransluscent
+        
+        barTintColor = .nearbyWeatherStandard
+        tintColor = .white
+        titleTextAttributes = [.foregroundColor: UIColor.white]
+        barStyle = .default
+    }
+}
