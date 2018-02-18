d2272f6e93154227ada23d99f3e29d00a5fd6b5a
erikmartens <erikmartens@me.com>
2018-02-19 00:02:59 +0100

    do db query async to not block main queue

--- NearbyWeather/WeatherLocationService.swift
+++ NearbyWeather/WeatherLocationService.swift
@@ -18,7 +18,7 @@ class WeatherLocationService {
     
     // MARK: - Private Assets
     
-    private let openWeatherMapCityServiceBackgroundQueue = DispatchQueue(label: "de.erikmaximilianmartens.nearbyWeather.openWeatherMapCityService", qos: DispatchQoS.background, attributes: [DispatchQueue.Attributes.concurrent], autoreleaseFrequency: .inherit, target: nil)
+    private let openWeatherMapCityServiceBackgroundQueue = DispatchQueue(label: "de.erikmaximilianmartens.nearbyWeather.openWeatherMapCityService", qos: .userInitiated, attributes: [.concurrent], autoreleaseFrequency: .inherit, target: nil)
     
     fileprivate let databaseQueue: FMDatabaseQueue
     
@@ -40,35 +40,37 @@ class WeatherLocationService {
         
         if searchString.count == 0 || searchString == "" { return completionHandler(nil) }
         
-        databaseQueue.inDatabase { database in
-            
-            let query = "SELECT * FROM locations WHERE (lower(name) LIKE '%\(searchString.lowercased())%')"
-            var queryResult: FMResultSet?
-            
-            do {
-                queryResult = try database.executeQuery(query, values: nil)
-            } catch {
-                print(error.localizedDescription)
-                return completionHandler(nil)
-            }
-            
-            guard let result = queryResult else {
-                completionHandler(nil)
-                return
-            }
-            
-            var retrievedLocations = [WeatherLocationDTO]()
-            while result.next() {
-                guard let location = WeatherLocationDTO(from: result) else {
+        openWeatherMapCityServiceBackgroundQueue.async {
+            self.databaseQueue.inDatabase { database in
+                
+                let query = "SELECT * FROM locations WHERE (lower(name) LIKE '%\(searchString.lowercased())%')"
+                var queryResult: FMResultSet?
+                
+                do {
+                    queryResult = try database.executeQuery(query, values: nil)
+                } catch {
+                    print(error.localizedDescription)
+                    return completionHandler(nil)
+                }
+                
+                guard let result = queryResult else {
+                    completionHandler(nil)
                     return
                 }
-                retrievedLocations.append(location)
+                
+                var retrievedLocations = [WeatherLocationDTO]()
+                while result.next() {
+                    guard let location = WeatherLocationDTO(from: result) else {
+                        return
+                    }
+                    retrievedLocations.append(location)
+                }
+                
+                guard !retrievedLocations.isEmpty else {
+                    return completionHandler(nil)
+                }
+                completionHandler(retrievedLocations)
             }
-            
-            guard !retrievedLocations.isEmpty else {
-                return completionHandler(nil)
-            }
-            completionHandler(retrievedLocations)
         }
     }
 }
